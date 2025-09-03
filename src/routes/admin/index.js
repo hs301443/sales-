254a983d24f199594276954804fb177842cf3fe4
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-09-03 12:54:23 +0300

    add Sources route

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import authRouter from "./auth.js";
 import userRouter from "./userRoutes.js";
 import targetRouter from "./targetRoutes.js";
+import sourceRouter from "./sourceRoutes.js";
 
 export const route = Router();
 
 route.use("/auth", authRouter);
 route.use("/users", userRouter);
 route.use("/targets", targetRouter);
+route.use("/sources", sourceRouter);
 
 
 export default route;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRouter from './user/index.js';
 import adminRouter from './admin/index.js';
 import authRouter from './auth/authRoutes.js';
+
 const route = Router();
 
 route.use('/auth', authRouter);
