8a090299cbfa8d1c3935a58c98ca42ec1b13d40e
Ahmed <Wegostationdev@gmail.com>
2025-09-06 10:56:36 +0200

    commit

--- src/controllers/Leader/LeadController.js
+++ src/controllers/Leader/LeadController.js
@@ -1,3 +1,4 @@
+import User from '../../models/modelschema/User.js'; 
 import Lead from '../../models/modelschema/Lead.js'; 
 import asyncHandler from 'express-async-handler';
 import { SuccessResponse, ErrorResponse } from '../../utils/response.js';
@@ -35,4 +36,25 @@ export const viewLead = asyncHandler(async (req, res) => {
   } catch (error) {
     return ErrorResponse(res, error.message, 400);
   }
+});
+
+export const transferLead = asyncHandler(async (req, res) => {
+  try {
+      const myId = req.currentUser.id;
+      const { userId } = req.params.id;
+
+      const sales = await User.find({ leader_id: myId }).select("_id");
+
+      const salesIds = sales.map(s => s._id);
+
+      await Lead.updateMany(
+        { sales_id: { $in: salesIds } },
+        { $set: { sales_id: userId } }
+      );
+
+      res.json({ message: "Lead transfer success successfully" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Server error" });
+    }
 });
