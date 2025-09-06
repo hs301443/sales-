93ec0e7526641766ecbf2f914ca05a7e377b4421
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-09-06 14:38:45 +0300

    fix some issue in lead

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,18 @@ import { Router } from "express";
 
 import adminRouter from './admin/index.js';
 import authRouter from './auth/authRoutes.js';
+import homeRouter from './leader/homeRoutes.js';
+import leadRouter from './leader/leadRoutes.js';
 
 const route = Router();
 
 route.use('/auth', authRouter);
 
 route.use('/admin', adminRouter);
+
+route.use('/leader', homeRouter);
+
+route.use('/leader/leads', leadRouter);
 
 
 export default route;
