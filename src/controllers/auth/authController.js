2f1c3b2601e0b2ae4084d71087e60fa68603982c
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-10-05 12:32:00 +0300

    fix issue soft delete in login

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -6,7 +6,7 @@ import { SuccessResponse } from '../../utils/response.js';
 import  { generateJWT }  from '../../middlewares/generateJWT.js';
 
 
-export const login = asyncHandler(async (req, res) => {
+/*export const login = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
 
   if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
@@ -16,4 +16,16 @@ export const login = asyncHandler(async (req, res) => {
  const token = await generateJWT({id: user.id, role: user.role});
 
   return SuccessResponse(res, { message: 'User logged in successfully', token, role: user.role, name: user.name, email: user.email}, 200);
+});*/
+
+export const login = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ email: req.body.email, isDeleted: false });
+
+  if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+    throw new BadRequest('Incorrect email or password');
+  }
+
+  const token = await generateJWT({ id: user.id, role: user.role });
+
+  return SuccessResponse(res, { message: 'User logged in successfully', token, role: user.role, name: user.name, email: user.email }, 200);
 });
