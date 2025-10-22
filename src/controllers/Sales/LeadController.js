49a368fdcb6424018f81285a6dcbeea69fefdf75
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-10-22 11:34:46 +0300

    return id country with city

--- src/controllers/Sales/LeadController.js
+++ src/controllers/Sales/LeadController.js
@@ -620,6 +620,7 @@ export const getAllCountryAndCity = asyncHandler(async (req, res) => {
       name: true,
       country: {
         select: {
+          id: true
           name: true
         }
       }
