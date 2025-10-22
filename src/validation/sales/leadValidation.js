fdae6305a488234a804126d47a333fb3337cc9f4
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-10-22 12:10:25 +0300

    Update leadValidation.js

--- src/validation/sales/leadValidation.js
+++ src/validation/sales/leadValidation.js
@@ -3,8 +3,8 @@ import Joi from "joi";
 export const createleadValidation = Joi.object({ 
   name: Joi.string().required().trim(),
   phone: Joi.string().required().trim(),
-  country: Joi.string().required().trim(),
-  city: Joi.string().required().trim(),
+  country: Joi.number().required().trim(),
+  city: Joi.number().required().trim(),
   //sales_id: Joi.number().required(),
   activity_id: Joi.number().optional(),
   source_id: Joi.number().required(),
@@ -13,8 +13,8 @@ export const createleadValidation = Joi.object({
 export const updateleadValidation = Joi.object({
   name: Joi.string().optional().trim(),
   phone: Joi.string().optional().trim(),
-  country: Joi.string().optional().trim(),
-  city: Joi.string().optional().trim(),
+  country: Joi.number().optional().trim(),
+  city: Joi.number().optional().trim(),
   status: Joi.string().optional().valid('default','intersted', 'negotiation', 'demo_request', 'demo_done', 'reject', 'approve').trim(),
  // sales_id: Joi.number().optional(),
   activity_id: Joi.number().optional(),
